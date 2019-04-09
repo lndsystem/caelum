ea6e021fb81a03f50a7b49a2e5cc608eec893a6f
Leandro Coelho <leandroces@gmail.com>
2019-04-08 22:40:41 -0300

    add

---
+++ aulas/cmail/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //title = 'cmail';
+  private _isNewEmailFormOpen = false;
+  get isNewEmailFormOpen() {
+    return this._isNewEmailFormOpen;
+  }
+  toggleNewEmailForm() {
+    this._isNewEmailFormOpen = !this.isNewEmailFormOpen;
+  }
+}
