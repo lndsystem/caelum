023703d2ed86dcaf096d36410f48228fc0081924
lndsystem <angular8217@caelum114-04>
2019-04-09 22:49:25 -0300

    add

--- aulas/cmail/src/app/app.component.ts
+++ aulas/cmail/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Email } from './models/email';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //title = 'cmail';
+  readonly regexEmail = "^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
   private _isNewEmailFormOpen = false;
+  
+  email = new Email();
+  emailList = [];
+
+
   get isNewEmailFormOpen() {
     return this._isNewEmailFormOpen;
   }
   toggleNewEmailForm() {
     this._isNewEmailFormOpen = !this.isNewEmailFormOpen;
   }
+
+  handleNewEmail(formEmail: NgForm) {
+    this.emailList.push(this.email);
+    this.email = new Email();
+    formEmail.resetForm();
+    this.toggleNewEmailForm();
+  }
+
+  /*handleNewEmail(event: Event) {
+    event.preventDefault();
+    this.emailList.push(this.email);
+    this.email = {
+      destinatario: '',
+      assunto: '',
+      conteudo: ''
+    }
+  }*/
+
+  
 }
---
+++ aulas/cmail/src/app/models/email.ts
@@ -0,0 +1,6 @@
+export class Email {
+    checked = false;
+    destinatario = '';
+    assunto = '';
+    conteudo = '';
+}
